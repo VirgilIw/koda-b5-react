2328f4a6fd0a5942975886631909c0116dc69ce8
Virgil I Ambar <imanuelambar11@gmail.com>
2025-12-02 13:22:23 +0700

    feat: add feature minitask1

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Calculate from "./components/Calculate";
+import Header from "./components/Header";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Calculate />
+    </>
+  );
+}
+
+export default App;
