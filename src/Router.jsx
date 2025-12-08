f18b0ab635a3c3d016eee5feecbfcab253f514e9
Virgil I Ambar <imanuelambar11@gmail.com>
2025-12-08 22:24:41 +0700

    feat: updated router

--- src/Router.jsx
+++ src/Router.jsx
@@ -6,6 +6,8 @@ import About from "./pages/About";
 import NotFound from "./pages/NotFound";
 import Home from "./pages/Home";
 import FormReview from "./pages/FormReview";
+import Minitask7 from "./pages/Minitask7";
+import Detail from "./pages/Detail";
 
 const Router = () => {
   return (
@@ -14,7 +16,12 @@ const Router = () => {
       <Route path="/about" element={<About />} />
       <Route path="/contact" element={<Contact />} />
       <Route path="/form-review" element={<FormReview />} />
-      {/* RUte catch ALl */}
+
+      <Route path="/character">
+        <Route index element={<Minitask7 />} />
+        <Route path=":id" element={<Detail />} />
+      </Route>
+
       <Route path="*" element={<NotFound />} />
     </Routes>
   );
